94fcb9edb665dbfe97e0565ca966d90584111452
stepech <st3pech@protonmail.com>
2021-12-12 03:28:56 +0100

    feat(UI overhaul): Add buttons, channel management
    Rework of how bot works
    Support for individual channel creation/deletion
    Picking channels through selection menu
    Change library to supported one
    Change to dynamic channel picking

---
+++ interactions/InitHiddenMessage.py
@@ -0,0 +1,44 @@
+import asyncio
+
+import discord
+from discord import Interaction, ButtonStyle
+from discord.ui import View, Select, Button, button
+
+
+class CancelButton(Button):
+    def __init__(self):
+        super().__init__(label="Zrušit", style=ButtonStyle.danger)
+
+    async def callback(self, interaction: Interaction):
+        overwrite = {interaction.user: discord.PermissionOverwrite(send_messages=False)}
+        await interaction.message.edit(content="Kanál se smaže za pár sekund", view=None)
+        await interaction.channel.edit(topic="Deleting...", overwrites=overwrite)
+        asyncio.create_task(self.delete(interaction.channel))
+
+    async def delete(self, channel: discord.TextChannel):
+        await asyncio.sleep(10)
+        await channel.delete()
+
+
+class RelayChannelSelection(Select):
+    def __init__(self, channels: list[discord.TextChannel]):
+        options: Select.options = []
+        for channel in channels:
+            options.append(discord.SelectOption(label=channel.name))
+        super().__init__(options=options)
+
+    async def callback(self, interaction: Interaction):
+        overwrites = {interaction.user: discord.PermissionOverwrite(send_messages=True)}
+        await interaction.channel.edit(topic=self.values[0], overwrites=overwrites)
+        edit_view = View()
+        edit_view.add_item(CancelButton())
+        await interaction.message.edit(content="Cílový kanál nastaven na {0}, napiš zprávu:".format(self.values[0]),
+                                       view=edit_view)
+
+
+class HiddenChannelMessageView(View):
+    def __init__(self, channel_list: list[discord.TextChannel]):
+        super().__init__()
+        self.add_item(RelayChannelSelection(channel_list))
+        self.add_item(CancelButton())
+
--- main.py
+++ main.py
@@ -2,19 +2,13 @@ import os
 from config import Config
 import discord
 from discord.ext import commands
-from discord.utils import escape_mentions
+from interactions.InitExchangeButton import InitExchange
 import logging
 import re
 
-DEBUG = False
 VERSION = "DEV"
 
-if DEBUG:
-    loggingLevel = logging.INFO
-else:
-    loggingLevel = logging.ERROR
-
-logging.basicConfig(level=loggingLevel)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('DiscordBot')
 
 config: Config = Config(logger)
@@ -24,21 +18,19 @@ pattern = re.compile("^(.{3}):", re.IGNORECASE)
 bot = commands.Bot(command_prefix="!", owner_id=470490558713036801, intents=discord.Intents.all())
 guild: discord.Guild
 
-welcome: discord.Embed = discord.Embed(colour=discord.Color.orange())
-welcome.title = "Obfuscation bot"
-welcome.url = config.sourceLink
-welcome.description = "Ahoj, tenhle bot anonymně přeposílá veškeré zprávy v tomto kanále do cílového kanálu tvé volby.\n\
-Zprávy mohou obsahovat přílohy."
-welcome.set_footer(text=VERSION)
-welcome.add_field(
-    name="Formát zprávy",
-    value="Zprávy potřebují prefix ve formátu kódu předmětu a dvojtečky.\nEg. `pa1:Špagetový kód je překrásný`",
-    inline=False)
-welcome.add_field(
-    name="Certifikace",
-    value="Certified by <@249093978975109122> to be `@everyone` impenetrable\n*RIP everyone who found out the hard way*",
-    inline=False
-)
+
+def create_welcome_embed() -> discord.Embed:
+    welcome_embed: discord.Embed = discord.Embed(colour=discord.Color.orange())
+    welcome_embed.title = "Obfuscation bot"
+    welcome_embed.url = config.sourceLink
+    welcome_embed.description = "Ahoj, tenhle bot anonymně přeposílá veškeré zprávy v tomto kanále do cílového kanálu tvé volby.\n\
+    Zprávy mohou obsahovat přílohy."
+    welcome_embed.set_footer(text=VERSION)
+    welcome_embed.add_field(
+        name="Formát zprávy",
+        value="Zprávy potřebují prefix ve formátu kódu předmětu a dvojtečky.\nEg. `pa1:Špagetový kód je překrásný`",
+        inline=False)
+    return welcome_embed
 
 
 @bot.event
@@ -46,11 +38,17 @@ async def on_ready():
     logging.info("Joined as {0.name}".format(bot.user))
     global guild
     guild = await bot.fetch_guild(config.GuildId)
-    channel: discord.TextChannel = [channel for channel in await guild.fetch_channels() if channel.id == config.listeningChannel][0]
-    welcomeMessage: discord.Message = await channel.fetch_message(config.welcomeMessage)
-    await welcomeMessage.edit(content="", embed=welcome)
+    channel: discord.TextChannel = [channel for channel in await guild.fetch_channels() if channel.id == config.welcomeChannelId][0]
+    try:
+        welcome_message: discord.Message = await channel.fetch_message(config.welcomeMessageId)
+        await welcome_message.edit(content="",
+                                   embed=create_welcome_embed(),
+                                   view=InitExchange(config, bot))
+    except discord.NotFound:
+        await channel.send(embed=create_welcome_embed(),
+                           view=InitExchange(config, bot))
 
-
+"""
 @bot.event
 async def on_message(msg: discord.Message):
     if msg.author.id == bot.user.id:
@@ -84,5 +82,6 @@ async def on_message(msg: discord.Message):
         files.append(f)
     await webhook[0].send(content, files=files, avatar_url=avatar)
     await msg.delete()
+"""
 
 bot.run(token)
